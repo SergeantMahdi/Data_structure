91543898cb653df3c7cd71108d68742889341c46
Mahdi Sartipzadeh <mahdi.sartipzadeh@gmail.com>
2024-11-22 00:25:27 -0800

    ADD: Static Array

---
+++ Array/staticArray.h
@@ -0,0 +1,61 @@
+#pragma once
+
+template<typename _Type, size_t _Capacity>
+class Array {
+
+private:
+	_Type* m_array;
+	size_t m_size;
+private:
+	void outOfBoundary_excep() {
+		try {
+			throw std::length_error("the index is out of boundary");
+		}
+		catch (const std::exception& error) {
+			std::cerr << "Access Violation: " << error.what() << std::endl;
+			std::exit(0);
+		}
+	}
+public:
+
+	Array()
+		:m_array(new _Type[_Capacity]), m_size(0){}
+
+
+	size_t size() {
+		return m_size;
+	}
+
+	void push_back(const _Type data) {
+		if (m_size >= _Capacity) {
+			std::cout << "the array is full!";
+			return;
+		}
+		m_array[m_size++] = data;
+	}
+
+	void replace(const _Type& data, const _Type& target) {
+		for (int i = 0; i < m_size; i++) {
+			if (m_array[i] == target) {
+				m_array[i] = data;
+			}
+		}
+	}
+
+	void fill(const _Type& data) {
+		for (int i = 0; i < _Capacity ; i++) {
+				m_array[i] = data;
+				m_size++;
+		}
+	}
+
+	_Type& operator[](const int& index) {
+		if(index > _Capacity){
+			outOfBoundary_excep();
+		}
+
+		return m_array[index];
+	}
+
+
+};
